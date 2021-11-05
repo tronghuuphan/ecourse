e1701d7672b078baebfc0e2953d277684608a80d
tronghuuphan <pthuu91@gmail.com>
2021-11-05 11:35:44 +0700

    update query

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, CourseViewSet
 
 router = routers.DefaultRouter()
 router.register('categories', viewset=CategoryViewSet, basename='category')
+router.register('courses', viewset=CourseViewSet, basename='course')
 
 urlpatterns = router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,28 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import ListModelMixin
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Course
+from .serializers import CategorySerializer, CourseSerializer
+from .paginations import CoursePagination
 
 class CategoryViewSet(ListModelMixin, GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class CourseViewSet(ListModelMixin, GenericViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    pagination_class = CoursePagination
+
+    def get_queryset(self):
+        courses = Course.objects.filter(active=True)
+        q = self.request.query_params.get('q')
+        if q is not None:
+            courses = Course.objects.filter(subject__contains=q)
+        category_id = self.request.query_params.get('category_id')
+        if category_id is not None:
+            courses = Course.objects.filter(category_id=category_id)
+        return courses
     
 
 
